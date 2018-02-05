0cbddf9e92252914e428d1fa9b69747d6ae3e268
Michael Dorn <12156832+xtt13@users.noreply.github.com>
2018-02-05 23:19:05 +0100

    Build Fixes

--- renderer.js
+++ renderer.js
@@ -2,21 +2,9 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-console.log('HELLO WORLD');
-console.log(this);
-
 const ipc = require('electron').ipcRenderer
 
-// const asyncMsgBtn = document.getElementById('async-msg')
-
-// asyncMsgBtn.addEventListener('click', function () {
-//   ipc.send('asynchronous-message', 'ping')
-// })
-
 ipc.send('asynchronous-message', 'ping')
-console.log('HELLO WORLD 2');
-console.log(ipc);
-console.log(this);
 
 global.ipc = ipc;  
 
